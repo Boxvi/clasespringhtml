045d3e82ab24921c47eb824c30448a6237413adf
Boxvi <hermosokawai@gmail.com>
2022-10-18 19:37:55 -0500

    inserta Mensaje aquí

--- src/main/java/prueba/clase/controller/HomeController.java
+++ src/main/java/prueba/clase/controller/HomeController.java
@@ -1,4 +1,4 @@
-package prueba.clase;
+package prueba.clase.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +11,13 @@ public class HomeController {
     public String home(){
         return "redirect:https://futbollatam.com/esp/?home";
     }
+
+    @GetMapping("/home")
+    public String home2(){
+        return "forward:/app/index";
+    }
+
+
     /*
     @GetMapping("/")
     public String home(){
